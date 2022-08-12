7ecf171c8a36203916a9b6b9436ad1715aecac74
Amanda Freitas <amandaafreitas20@gmail.com>
2022-08-12 19:05:40 -0300

    Inserindo construtores e atributos estaticos

--- projeto estudos POO alura/ContaCorrente.js
+++ projeto estudos POO alura/ContaCorrente.js
@@ -1,7 +1,9 @@
 import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
+  static numeroDeContas = 0;
   agencia;
   _cliente;
+  _saldo = 0;
 
   set cliente(novoValor) {
     if (novoValor instanceof Cliente) {
@@ -13,10 +15,14 @@ export class ContaCorrente {
     return this._cliente;
   }
 
-  _saldo = 0;
-
   get saldo() {
     return this._saldo;
+  }
+
+  constructor(agencia, cliente) {
+    this.agencia = agencia;
+    this.cliente = cliente;
+    ContaCorrente.numeroDeContas += 1;
   }
 
   sacar(valor) {
